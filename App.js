f6d04cbb8a1ecb20fc9e2c346dd654848226d625
Muhammad Hamza <muhammadhamza2965@gmail.com>
2021-03-09 20:25:58 +0500

    Update App.js
    Code re-written with one command for saving username - nickname - avatar & roles in the db

--- App.js
+++ App.js
@@ -1,112 +1,82 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({ ws: { intents: Discord.Intents.ALL } });
 const mongoose = require("mongoose");
 const db = require("./Schema/db");
 var sizeof = require("object-sizeof");
 const { token, prefix } = require("./config/keys");
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+var counter = 0;
+//routes setup
+app.use("/", routes);
+
 //Connect databse
 mongoose.connect(
   "mongodb://localhost:27017/dbot",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log("DB connected!");
+    console.log("DB HAS BEEN CONNECTED...");
   }
 );
 
-client.once("ready", () => {
-  console.log("Ready!");
-});
-
-client.on("message", (message) => {
-  if (message.content === `${prefix}sync`) {
-    let ids = [];
-    let usernames = [];
-    let roles = [];
-    let role_id = [];
-    let nickname = [];
-    let obj = [];
-    var data;
-    client.users.cache.map((user) => {
-      ids.push(user.id); //get id of one user
-      usernames.push(user.username); //get username of one user
-      role_id.push(message.guild.members.cache.get(user.id)._roles); //get roles with id of one user
-      nickname.push(message.guild.members.cache.get(user.id).nickname); //get nickname of one user
-    });
-
-    //Algo to save roles nickname and username in one obj.
-    for (var j = 0; j < ids.length; j++) {
-      obj.push([usernames[j], nickname[j], role_id[j]]);
-    }
-    //Save username nickname roles in database
-    for (let i = 0; i < obj.length; i++) {
-      //check if users are already there then return else save
-      let doc;
-      db.find({ nickname: obj[i][1] }).then((data) => {
-        if (sizeof(data) == 0) {
-          //save users to db
-          new db({
-            username: obj[i][0],
-            nickname: obj[i][1],
-            roles: obj[i][2],
-          })
-            .save()
-            .then((data) => {
-              console.log(data);
-            })
-            .catch((error) => {
-              throw error;
-              message.channel.send(error);
+client.on("message", (msg) => {
+  if (msg.content == `${prefix}updateUsers`) {
+    msg.guild.members.fetch().then((member) => {
+      member.forEach((e) => {
+        db.exists({ username: e.user.username }, (err, bool) => {
+          //keep track of users and increase counter
+          counter++;
+          if (bool == true) {
+            //means user exists then update
+            //update in db
+            db.findOneAndUpdate(
+              { username: e.user.username },
+              {
+                username: e.user.username,
+                nickname: e.nickname,
+                roles: msg.guild.member(e.user)._roles,
+                avatar: e.user.displayAvatarURL(),
+              }
+            ).then((doc) => {
+              console.log(doc, " user updated!");
             });
-        } else {
-          console.log("User already exists");
-        }
-      });
-    }
-    message.channel.send("User synchronized!");
-  }
-});
-
-//Updating roles
-client.on("message", (message) => {
-  if (message.content === `${prefix}syncroles`) {
-    let role_id = [];
-    let usernames = [];
-    let obj = [];
-    client.users.cache.map((user) => {
-      usernames.push(user.username); //get username of one user
-      role_id.push(message.guild.members.cache.get(user.id)._roles); //get roles with id of one user
-    });
-
-    //Algo to save roles and username in one obj.
-    for (var j = 0; j < role_id.length; j++) {
-      obj.push([usernames[j], role_id[j]]);
-    }
-
-    for (let i = 0; i < obj.length; i++) {
-      //Find user by username and update the roles
-      db.findOneAndUpdate(
-        {
-          username: usernames[i], // search by username
-        },
-        {
-          roles: role_id[i], // Update roles
-        },
-        {
-          new: true, // return updated doc
-          runValidators: true, // validate before update
-        }
-      )
-        .then((doc) => {
-          console.log("Roles updated!");
-        })
-        .catch((err) => {
-          console.error(err);
-          message.channel.send(err);
+          }
+          //if user does not exists
+          if (bool == false) {
+            db({
+              username: e.user.username,
+              nickname: e.nickname,
+              roles: msg.guild.member(e.user)._roles,
+              avatar: e.user.displayAvatarURL(),
+            })
+              .save()
+              .then((doc) => {
+                console.log(doc, " user saved");
+              });
+          }
+          //if error
+          if (err) {
+            console.log(err);
+            msg.channel.send("An error occured");
+          }
         });
-    }
-    message.channel.send("Roles Updated!");
+      });
+    }).then(()=>{
+      msg.channel.send(` Updating users...`)
+    })
   }
-  
 });
 
+//get ready the bot
+client.once("ready", () => {
+  console.log("Bot Is Ready!");
+});
+
+//start the bot
 client.login(token);
+
+//start the server
+app.listen(3000, () => {
+  console.log("SERVER IS UP!");
+});
